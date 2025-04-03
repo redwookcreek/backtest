4090ac25db43d9559b1d650c267090fec6e958d6
redwookcreek <167585248+redwookcreek@users.noreply.github.com>
2025-04-02 21:01:37 -0700

    Do not call norgate data updater

--- update_data.ps1
+++ update_data.ps1
@@ -1,5 +1,5 @@
-& 'C:\Program Files\Norgate Data Updater\bin\ndu.trigger.exe' UPDATE WAIT
-Set-Location C:\Users\liu_w\OneDrive\Documents\zipline\backtest
+# & 'C:\Program Files\Norgate Data Updater\bin\ndu.trigger.exe' UPDATE WAIT
+# Set-Location C:\Users\liu_w\OneDrive\Documents\zipline\backtest
 .\start-env.ps1
 zipline clean -b norgatedata-all-us -k 1
 zipline ingest -b norgatedata-all-us
